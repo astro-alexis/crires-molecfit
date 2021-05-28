b837eb4d691374a4eccb6ee0516637b8bcd9666b
astro-alexis <alexis.lavail@physics.uu.se>
2021-05-28 16:42:06 +0200

    config that works for K221 at least

--- molecfit.sh
+++ molecfit.sh
@@ -1,3 +1,3 @@
 esorex --output-dir=MODEL --recipe-config=model.rc molecfit_model model.sof
 esorex --output-dir=CALCTRANS --recipe-config=calctrans.rc molecfit_calctrans calctrans.sof
-#esorex --output-dir=CORRECT --recipe-config=correct.rc molecfit_correct correct.sof 
+esorex --output-dir=CORRECT --recipe-config=correct.rc molecfit_correct correct.sof 
